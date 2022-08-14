89b422a053076ac34d81af11c4903f374c59a79c
Webmastee <103311266+Webmastee@users.noreply.github.com>
2022-08-14 22:06:02 +0100

    Add files via upload

---
+++ js/loader_claim.js
@@ -0,0 +1,58 @@
+
+
+
+document.getElementById("claimButton").innerText = claimPageInfo.claimButtonText;
+
+
+    // let claimbuttons = document.querySelectorAll(".claimButton");
+
+    // for (let i =0; i<claimbuttons.length; i++){
+    //   claimbuttons[i].innerText = claimPageInfo.claimButtonText;
+    // }
+
+
+
+
+
+
+
+
+
+// document.getElementById("cp_name").innerHTML = collectionInfo.name;
+// if (collectionInfo.socialMedia.discord == "") {
+//     document.head.insertAdjacentHTML('beforeend', '<style> #social_discord { opacity: 0.5; pointer-events: none; } </style>');
+// } else document.getElementById("social_discord").href = collectionInfo.socialMedia.discord;
+// if (collectionInfo.socialMedia.twitter == "") {
+//     document.head.insertAdjacentHTML('beforeend', '<style> #social_twitter { opacity: 0.5; pointer-events: none; } </style>');
+// } else document.getElementById("social_twitter").href = collectionInfo.socialMedia.twitter;
+// if (collectionInfo.socialMedia.instagram == "") {
+//     document.head.insertAdjacentHTML('beforeend', '<style> #social_instagram { opacity: 0.5; pointer-events: none; } </style>');
+// } else document.getElementById("social_instagram").href = collectionInfo.socialMedia.instagram;
+
+// document.title = collectionInfo.name;
+
+// document.getElementById("titleH2").innerHTML = claimPageInfo.title;
+// document.getElementById("shortDescription").innerHTML = claimPageInfo.shortDescription;
+// document.getElementById("longDescription").innerHTML = claimPageInfo.longDescription;
+
+
+
+// document.getElementById("badgeImage").src = `./assets/${claimPageInfo.image}`;
+// document.getElementById("badgeImage").style.borderRadius = claimPageInfo.imageRadius + "px";
+
+// document.title = collectionInfo.name;
+
+// document.getElementById("titleH2").innerHTML = claimPageInfo.title;
+// document.getElementById("shortDescription").innerHTML = claimPageInfo.shortDescription;
+// document.getElementById("longDescription").innerHTML = claimPageInfo.longDescription;
+
+//     let claimbuttons = document.querySelectorAll(".claimButton");
+
+//     for (let i =0; i<claimbuttons.length; i++){
+//       claimbuttons[i].innerText = claimPageInfo.title;
+//     }
+
+//     document.getElementById("badgeImage").src = `./assets/${claimPageInfo.image}`;
+//     document.getElementById("badgeImage").style.borderRadius = claimPageInfo.imageRadius + "px";
+
+
